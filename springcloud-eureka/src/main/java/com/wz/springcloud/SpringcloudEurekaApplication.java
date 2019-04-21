342bda5197a25a863352b7c49864d5887de11901
wangzhi <1141532572@qq.com>
2019-04-21 11:24:39 +0800

    注册中心部分未提交代码

--- springcloud-eureka/src/main/java/com/wz/springcloud/SpringcloudEurekaApplication.java
+++ springcloud-eureka/src/main/java/com/wz/springcloud/SpringcloudEurekaApplication.java
@@ -5,11 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaServer  // 声明是Eureka服务端
 public class SpringcloudEurekaApplication {
 
 	public static void main(String[] args) {
+		// devtools代码更新重启
+//		System.setProperty("spring.devtools.restart.enabled", "true");
 		SpringApplication.run(SpringcloudEurekaApplication.class, args);
+		System.out.println(11111111);
+		System.out.println(3434);
+
 	}
 
 }
